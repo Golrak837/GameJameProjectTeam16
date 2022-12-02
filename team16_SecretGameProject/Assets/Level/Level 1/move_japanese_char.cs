636c1775be56dfa48f7a9e04693040a1cfba795d
Ethalides33 <amauryba33@icloud.com>
2022-12-02 21:11:23 +0100

    Physics for platform movement and switch script

---
+++ team16_SecretGameProject/Assets/Level/Level 1/move_japanese_char.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_japanese_char : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody2D rb2d;
+    [SerializeField] private Vector2 velocity = Vector2.zero;
+    public bool isJumping;
+    public bool isGrounded;
+    float _horizontalMovement;
+    public Transform GroundCheckLeftJ;
+    public Transform GroundCheckRightJ;
+    public SpriteRenderer SpriteRendererJ;
+    public Animator animatorJ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.Space))
+            {
+                isJumping=true;
+            }       
+    }
+
+    void FixedUpdate()
+    {      
+
+        _horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        isGrounded = Physics2D.OverlapArea(GroundCheckLeftJ.position,GroundCheckRightJ.position);
+
+
+        Movepl(_horizontalMovement);
+        Flip(rb2d.velocity.x);
+        
+        animatorJ.SetFloat("Speed",Mathf.Abs(rb2d.velocity.x));
+    }
+
+    void Movepl(float _horizontalMovement)
+    {
+        // Debug.Log("moveplayer");
+        Vector2 targetVelocity = new Vector2(_horizontalMovement,rb2d.velocity.y);
+        rb2d.velocity = Vector2.SmoothDamp(rb2d.velocity,targetVelocity, ref velocity, .05f);        
+
+        if(isJumping && isGrounded){
+            rb2d.AddForce(new Vector2(0f,jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity){
+
+        if (_velocity > 0.1f){ // entre -1 et 1
+            SpriteRendererJ.flipX=false;
+            }
+        else if(_velocity < -0.1f){
+            SpriteRendererJ.flipX=true;
+            }
+        }
+
+}
