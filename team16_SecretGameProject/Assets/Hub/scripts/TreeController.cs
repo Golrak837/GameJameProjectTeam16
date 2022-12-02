adea8b4bdd2fa0bfaa845baa1e50f2b70968e6c2
Golrak837 <maxime.ferriere@student.vinci.be>
2022-12-02 23:27:08 +0100

    Etat player init

--- team16_SecretGameProject/Assets/Hub/scripts/TreeController.cs
+++ team16_SecretGameProject/Assets/Hub/scripts/TreeController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class TreeController : MonoBehaviour
 {
     [SerializeField] public LayerMask playerLayer;
+    [SerializeField] public Transform tree;
+    private int stateArbre;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (playerLayer != (playerLayer | 1 << col.gameObject.layer)) return;
-        Destroy(gameObject);        
+        int nbrElement;
+        var dam = col.gameObject.GetComponent<EtatElementPlayer>();
+        if (dam != null)
+        {
+             nbrElement = dam.nbrEtatElement;
+             if (nbrElement > stateArbre)
+             {
+                 Debug.Log("Etat :" + nbrElement);
+                 stateArbre = nbrElement;
+                 if(nbrElement==5) Debug.Log("You save the Planet!");
+             }
+        }
+        
 
     }
 }
