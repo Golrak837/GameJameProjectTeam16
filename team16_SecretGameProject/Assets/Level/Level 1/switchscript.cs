f002792a5e18038ee44a15b5658cad28c9eb7bb2
Golrak837 <maxime.ferriere@student.vinci.be>
2022-12-03 10:21:23 +0100

    init cameraCinemachine in lvl1 and recreate camera in hub

--- team16_SecretGameProject/Assets/Level/Level 1/switchscript.cs
+++ team16_SecretGameProject/Assets/Level/Level 1/switchscript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class switchscript : MonoBehaviour
 {
     // Start is called before the first frame update
- 
      public move_japanese_char PlayerA;
      public move_belgian_char PlayerB;
  
