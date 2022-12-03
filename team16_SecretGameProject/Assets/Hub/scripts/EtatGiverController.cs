b40e076cd165ce16d0264573d09e0154e5c3abbf
Golrak837 <maxime.ferriere@student.vinci.be>
2022-12-03 17:00:52 +0100

    add fonction element

--- team16_SecretGameProject/Assets/Hub/scripts/EtatGiverController.cs
+++ team16_SecretGameProject/Assets/Hub/scripts/EtatGiverController.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class EtatGiverController : MonoBehaviour
 {
     [SerializeField] public LayerMask playerLayer;
+    [SerializeField] public LayerMask player2Layer;
     [SerializeField] public int etat;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (playerLayer != (playerLayer | 1 << col.gameObject.layer)) return;
-        var dam = col.gameObject.GetComponent<EtatElementPlayer>();
-        dam?.SetElement(etat);
+        if ((playerLayer != (playerLayer | 1 << col.gameObject.layer))&&(player2Layer != (player2Layer | 1 << col.gameObject.layer))) return;
+        EtatElementPlayer.instance.SetElement(etat);
+        
     }
 }
