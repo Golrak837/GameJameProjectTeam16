433d8d3aa17955732be093b5f52ef7f0e5efe15d
Ethalides33 <amauryba33@icloud.com>
2022-12-04 23:21:02 +0100

    trying hub movement and animation implementation

--- team16_SecretGameProject/Assets/Hub/scripts/MovementPlayer.cs
+++ team16_SecretGameProject/Assets/Hub/scripts/MovementPlayer.cs
@@ -7,9 +7,11 @@ public class MovementPlayer : MonoBehaviour
     private Vector3 direction;
     [SerializeField] private float speed;
     private bool m_FarcingRight = true;
+    private bool m_FarcingTop = true ;
     public AudioSource audioSource;
     public Rigidbody2D rb2d;
     [SerializeField] private Vector2 velocity = Vector2.zero;
+    public Animator animator;
 
     void Start(){
         rb2d = GetComponent<Rigidbody2D> ();
@@ -28,20 +30,41 @@ public class MovementPlayer : MonoBehaviour
         var deplacement = direction * speed * Time.deltaTime;
         if ((deplacement.x < 0 && m_FarcingRight) || (deplacement.x > 0 && !m_FarcingRight))
         {
-            audioSource.Play();
-            Flip();
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+            Flipx();
+            animator.SetBool("isMoving",true);
         }
+
+        if ((deplacement.y < 0 && m_FarcingTop) || (deplacement.x > 0 && !m_FarcingTop))
+            {
+                if(!audioSource.isPlaying){
+                    audioSource.Play();
+                }
+
+                Flipy();
+                animator.SetBool("isMoving",true);
+            }
+
         else if (deplacement.x==0 && deplacement.y ==0){
                 audioSource.Stop();
+            animator.SetBool("isMoving",false);
 
         }
         
         transform.position += deplacement;
     }
     
-    private void Flip()
+    private void Flipx()
     {
         m_FarcingRight = !m_FarcingRight;
         transform.Rotate(0f,180f,0f);
+    }
+
+        private void Flipy()
+    {
+        m_FarcingTop = !m_FarcingTop;
+        transform.Rotate(0f,0f,180f);
     }
 }
